fcdcb63ef054d93ad5bfd2270255e7e453cd9b2c
CKofficial-1797 <sanjaygorain1797@gmail.com>
2026-02-16 08:15:48 +0530

    feat: add app layout and landing page

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="max-w-2xl mx-auto px-4 my-10 sm:px-6 lg:px-8">
+      <h1 className="text-4xl my-4 font-bold">
+        PCON Backend working properly!!
+      </h1>
+    </div>
+  );
+}
